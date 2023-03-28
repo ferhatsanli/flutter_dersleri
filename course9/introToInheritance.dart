cd9656fd4445e8166daa0ca99453dfbc82b5e42d
Ferhat Sanli <ferhatsanli.sea@gmail.com>
2023-03-28 18:27:35 +0300

    upscaling

--- course9/introToInheritance.dart
+++ course9/introToInheritance.dart
@@ -7,7 +7,21 @@ void main(List<String> args) {
   norUser.login();
   roUser.login();
   roUser.sayMyName();
+
+  // upcasting
+  User admin = AdminUser();
+  admin.login();
+  List<User> allUsers = [];
+  allUsers.add(admin);
+  allUsers.add(parUser);
+  allUsers.add(norUser);
+  allUsers.add(roUser);
+
+  // logged in to all users owing to the upscaling
+  for (User u in allUsers) test(u);
 }
+
+void test(User u) => u.login();
 
 class User {
   String uid = 'ferhat';
@@ -18,9 +32,22 @@ class User {
   }
 }
 
+class AdminUser extends User{
+  @override
+  String get uid => "admin";
+  @override
+  String get psw => "nimda";
+
+  @override
+  void login() {
+    print("Logging in as admin user.");
+  }
+}
+
 class NormalUser extends User {
   @override
   void login() {
+    super.login; // parent class function
     print("Logging in as normal user.");
   }
 }
